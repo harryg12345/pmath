2b9d27db476955ed21561c463962584763f4c325
harryg12345 <132217767+harryg12345@users.noreply.github.com>
2023-09-22 18:37:26 +0100

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,9 @@
+addUser(); {
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1_key", player1_name);
+    localStorage.setItem("player2_key", player2_name);
+
+    window.location = "quiz_game_page.html";
+}
+
